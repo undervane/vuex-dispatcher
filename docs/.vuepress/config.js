335c318432ff6c8ef73d406abf75896b37f179b8
Undervane <sergio@mipigu.com>
2019-09-30 15:44:58 +0200

    docs(docs-integration) Add new nvabar links

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  title: 'Vuex Dispatcher',
-  description: '🌈 An easy-to-use payload builder for your dispatch actions',
+  title: '🌈 Vuex Dispatcher',
+  description: 'An easy-to-use payload builder for your dispatch actions',
   themeConfig: {
+    nav: [
+      { text: 'Github Repo', link: 'https://github.com/undervane/vuex-dispatcher' },
+      { text: 'Author', link: 'https://mipigu.com' }
+    ],
     sidebar: [
       '/getting-started/',
       '/example-usage/',
